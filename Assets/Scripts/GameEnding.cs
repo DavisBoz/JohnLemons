7560bd946cf99ba18b4cbe0df3b379bc55dba2dc
Davis Bosworth <dboswor2@uoregon.edu>
2020-04-14 17:49:41 -0700

    allll done

---
+++ Assets/Scripts/GameEnding.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameEnding : MonoBehaviour
+{
+    public float fadeDuration = 1f;
+    public float displayImageDuration = 1f;
+    public GameObject player;
+    public GameObject other;
+    public CanvasGroup exitBackgroundImageCanvasGroup;
+    public AudioSource exitAudio;
+    public CanvasGroup caughtBackgroundImageCanvasGroup;
+    public AudioSource caughtAudio;
+
+    bool m_IsPlayerAtExit;
+    bool m_IsPlayerCaught;
+    float m_Timer;
+    bool m_HasAudioPlayed;
+
+
+    public void CaughtPlayer()
+    {
+        m_IsPlayerCaught = true;
+    }
+
+    void Update()
+    {
+        Vector3 dirFromAtoB = (player.transform.position - other.transform.position).normalized;
+        float dotProd = Vector3.Dot(dirFromAtoB, other.transform.forward);
+
+        if (dotProd > 0.95)
+        {
+            m_IsPlayerAtExit = true;
+        }
+            if (m_IsPlayerAtExit)
+        {
+            EndLevel(exitBackgroundImageCanvasGroup, false, exitAudio);
+        }
+        else if (m_IsPlayerCaught)
+        {
+            EndLevel(caughtBackgroundImageCanvasGroup, true, caughtAudio);
+        }
+    }
+
+    void EndLevel(CanvasGroup imageCanvasGroup, bool doRestart, AudioSource audioSource)
+    {
+        if(!m_HasAudioPlayed)
+        {
+            audioSource.Play();
+            m_HasAudioPlayed = true;
+        }
+
+        m_Timer += Time.deltaTime;
+        imageCanvasGroup.alpha = m_Timer / fadeDuration;
+
+        if (m_Timer > fadeDuration + displayImageDuration)
+        {
+            if (doRestart)
+            {
+                SceneManager.LoadScene(0);
+            }
+            else
+            {
+                Application.Quit();
+            }
+        }
+    }
+}
+
+
---
+++ Assets/Scripts/Lerp.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lerp : MonoBehaviour
+{
+    public Transform mTarget;
+    public float mTravelTime;
+
+    Vector3 mStartPos;
+    Vector3 mTargetPos;
+
+    float mTimer;
+
+    void Start()
+    {
+        mStartPos = transform.position;
+        mTargetPos = mTarget.position;
+        mTimer = 0.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mTimer += Time.deltaTime;
+        transform.position = Vector3.Lerp(mStartPos, mTargetPos, mTimer / mTravelTime);
+    }
+}
